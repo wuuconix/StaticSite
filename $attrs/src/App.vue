39b4b68d045d09a0eb9c176d06e681e1edd9d2f4
wuuconix <80732538+wuuconix@users.noreply.github.com>
2022-04-29 10:05:48 +0800

    添加$attrs 学习$attrs来传递非props的属性以及父组件的方法

---
+++ $attrs/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="wrapper">
+        <child class="test-class" id="test-id" wuuconix="yyds" @click="child_click"></child>
+    </div>
+</template>
+
+<script>
+import child from './components/Child.vue'
+
+export default {
+    components: {
+        child
+    },
+    methods: {
+        child_click() {
+            console.log("child is clicked")
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
---
+++ $attrs/src/components/Child.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="wrappper">
+        按F12查看$attrs
+    </div>
+</template>
+
+<script>
+
+export default {
+    mounted() {
+        console.log(`this.$attrs: ${this.$attrs}`)
+        console.log(`this.$attrs["id"]: ${this.$attrs["id"]}`)
+        console.log(`this.$attrs["class"]: ${this.$attrs["class"]}`)
+        console.log(`this.$attrs["wuuconix"]: ${this.$attrs["wuuconix"]}`)
+        console.log(`this.$attrs["onClick"]: ${this.$attrs["onClick"]}`)
+        console.log("作为子组件我要调用父组件的函数了！")
+        this.$attrs["onClick"]() //自己调用父组件
+    },
+    props: ["wuuconix"]
+}
+</script>
+
+
+<style scoped>
+    div {
+        width: 50vw;
+        height: 75vh;
+        background-color: purple;
+        text-align: center;
+        line-height: 75vh;
+        color: cornsilk
+    }
+</style>
