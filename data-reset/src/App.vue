f66c95d7862ac41c697079a15b02fc56952b6fc9
wuuconix <80732538+wuuconix@users.noreply.github.com>
2022-04-29 13:35:14 +0800

    添加data-reset 学习如何重置data this.$options.data(this)

---
+++ data-reset/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1> {{ title }}</h1>
+        <img :src="src">
+        <button @click="change">换一张图</button>
+        <button @click="reset">重置data</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            src: "https://url.wuuconix.link/favicon",
+            title: "重置data学习"
+        }
+    },
+    methods: {
+        change() {
+            this.src = `https://conix.ml/?random=${Math.random()}`
+            this.title = this.src
+        },
+        reset() {
+            // this.$data = this.$options.data(this)
+            console.log(`this.$options.data(this):`)
+            console.log(this.$options.data(this))
+            Object.assign(this.$data, this.$options.data(this))
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    button {
+        margin-bottom: 15px;
+    }
+    img {
+        width: 40%;
+    }
+</style>
