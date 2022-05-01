bbd1e192a544c9136bef8abd1ac82e65362987be
wuuconix <80732538+wuuconix@users.noreply.github.com>
2022-05-01 18:02:17 +0800

    增加cache-control src源文件

---
+++ cache-control/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="outer_wrapper">
+        <h3>浏览器缓存Cache-Control学习</h3>
+        <div class="inner_wrapper">
+            <Son src="https://conix.ml" description="cache-control: max-age=31536000"></Son>
+            <Son src="https://conix.ml/?no-store=1" description="cache-control: no-store"></Son>
+        </div>
+        <button @click="fresh">刷新页面查看缓存情况</button>
+    </div>
+
+</template>
+
+<script>
+import Son from './components/Son.vue'
+
+export default {
+    components: {
+        Son
+    },
+    methods: {
+        fresh() {
+            window.location.reload()
+        }
+    }
+}
+</script>
+
+<style scoped>
+    div.outer_wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    div.inner_wrapper {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    Son {
+        margin: 0px 20px;
+    }
+</style>
---
+++ cache-control/src/components/Son.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <img :src="src">
+        <a :href="src"> {{ src }} </a>
+        <p>{{ description }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["src", "description"]
+}
+</script>
+
+<style scoped>
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+    }
+    img {
+        width: 50%;
+    }
+    p {
+        text-align: center;
+    }
+</style>
