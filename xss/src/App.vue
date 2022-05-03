c59136d97a9b95b29de3cee66fc2811e57a1f52f
wuuconix <80732538+wuuconix@users.noreply.github.com>
2022-05-03 16:22:16 +0800

    添加xss 学习xss可以干的事情

---
+++ xss/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="wrapper">
+        <div v-html="html"></div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            html: "<h1>这是用Vue写的XSS测试页</h1>"
+        }
+    },
+    mounted() {
+        document.cookie = "Cookie{Your_Secret_Cookie}"
+        let q = new URL(window.location.href).searchParams.get("q")
+        if (q) {
+            this.html = q
+            console.log(q)
+        }
+    }
+}
+</script>
+
+<style>
+    div.wrapper {
+        width: 100%;
+        height: 80vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
